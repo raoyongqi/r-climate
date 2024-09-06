f49c8359c1662cd8e167412e43a8a2347dce785e
r <1152416230@qq.com>
2024-09-06 02:49:59 -0400

    090603

--- merge1.r
+++ merge1.r
@@ -1,4 +1,33 @@
 # 检查对象是否存在
+if (!require(dplyr)) install.packages("dplyr")
+library(dplyr)
+library(openxlsx)
+sys_info <- Sys.info()
+if (!is.null(sys_info)) {
+  if (sys_info['sysname'] == 'Windows') {
+    print("系统是Windows")
+    setwd("C:/Users/r/Desktop/r_climate/data")
+    
+  } else if (sys_info['sysname'] == 'Linux') {
+    os_release <- readLines('/etc/os-release')
+    if (any(grepl("Ubuntu", os_release))) {
+      print("系统是Ubuntu")
+      setwd("~/Desktop/getData/r-climate/data")
+      
+      
+    } else {
+      print("系统是其他Linux发行版")
+    }
+  } else {
+    print("系统不是Windows或Ubuntu")
+  }
+} else {
+  print("无法获取系统信息")
+}
+
+
+
+
 if (!exists("result")) {
   # 如果对象不存在，则从Excel文件中读取数据
   # 需要先安装并加载readxl包
@@ -10,14 +39,23 @@ if (!exists("result")) {
 }
 
 site_data <- read_excel("1_Alldata.xlsx", sheet = "Plotdata")
+# 去掉 'site' 列（如果存在）
+if ("site" %in% names(site_data)) {
+  site_data <- site_data %>%
+    select(-site)
+}
 
+# 将 'Site' 列重命名为 'site'（如果存在）
+if ("Site" %in% names(site_data)) {
+  site_data <- site_data %>%
+    rename(site = Site)
+}
 names(result) <- ifelse(
   tolower(names(result)) %in% c("lon", "lat"),
   toupper(names(result)),
   names(result)
 )
-if (!require(dplyr)) install.packages("dplyr")
-library(dplyr)
+
 merged_df <- site_data %>%
   left_join(result, by = "site")
 
